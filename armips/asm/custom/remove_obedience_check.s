87289b07fb316e1fad8f5a624f46f778a8fdc214
memory5ty7 <amaury.willem57@gmail.com>
2025-09-08 21:47:20 +0200

    drayano patches

---
+++ armips/asm/custom/remove_obedience_check.s
@@ -0,0 +1,14 @@
+.nds
+.thumb
+
+// Credits to Drayano60
+// Aurora Crystal specific change
+// Removes traded Pokémon being able to disobey
+// Changes check for >= 8 badges to >= 0 badges
+
+.open "base/overlay/overlay_0012.bin", 0x022378C0
+
+.org 0x0224AF64
+.byte 0
+
+.close
